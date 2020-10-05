b2a44cd4b453e65038fd4e05068bf514856437f4
Bjoel-coder <bjoel.cuper@mail.ru>
2020-10-05 17:06:32 +0300

    created ui in Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from Sources.App import Ui_MainWindow
 import sys
 import time
 import os
 from os import remove
 from random import choice
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+
+
+app = QApplication([])
+application = MainWindow()
+qstyle = open("Sources/style.css", "r")
+application.setStyleSheet(qstyle.read())
+application.show()
+
+sys.exit(app.exec())
