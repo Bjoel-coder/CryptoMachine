387c8482effd97b9daffa63b0153f79717611a42
Bjoel-coder <bjoel.cuper@mail.ru>
2020-10-21 04:14:27 +0300

    many changes Added: 1)button "action" 2)button "how work" 3)comboBox "select shifr" 4)lineEdit "input key" 5)textEdit "output text" 6)textEdit "output key" 7)rework animations "buttons action" 8)rework animations "input key", "ouput key" and "output text"

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QPropertyAnimation, QRect
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from Sources.Gui.Gui_py import Ui_MainWindow
 import sys
@@ -13,6 +14,141 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+        self.but_encode_press = False
+
+        self.but_history.clicked.connect(self.btnHistory)
+        self.but_close.clicked.connect(self.btnClose)
+        self.dark_background.clicked.connect(self.btnClose)
+        self.but_act_encode.clicked.connect(self.selectAction)
+        self.but_act_decode.clicked.connect(self.selectAction)
+        self.but_act.clicked.connect(self.encodeDecode)
+
+
+
+    def selectAction(self):
+        ###
+        if self.action == "Зашифровать":
+            self.action = "Расшифровать"
+
+            #Animation for button action decode
+            self.anim_but_act_decode = QPropertyAnimation(self.but_act_decode, b"geometry")
+            self.anim_but_act_decode.setEndValue(QRect(212, 4, 250, 36))
+            self.anim_but_act_decode.setDuration(200)
+
+            #Animation for button action encode
+            self.anim_but_act_encode = QPropertyAnimation(self.but_act_encode, b"geometry")
+            self.anim_but_act_encode.setEndValue(QRect(178, 4, 36, 36))
+            self.anim_but_act_encode.setDuration(200)
+
+            #Animation for line input key
+            self.anim_line_inp_key = QPropertyAnimation(self.line_inp_key, b"geometry")
+            self.anim_line_inp_key.setEndValue(QRect(60, 159, 520, 36))
+            self.anim_line_inp_key.setDuration(200)
+
+            #Animation for button action
+            self.anim_but_act = QPropertyAnimation(self.but_act, b"geometry")
+            self.anim_but_act.setEndValue(QRect(60, 199, 520, 36))
+            self.anim_but_act.setDuration(200)
+
+            #starting animations
+            self.anim_but_act_encode.start()
+            self.anim_but_act_decode.start()
+            self.anim_line_inp_key.start()
+            self.anim_but_act.start()
+
+            #moving line_out_key and line_out_text off the screen
+            self.line_out_text.move(60, 386)
+            self.line_out_key.move(60, 497)
+
+            #set text
+            self.but_act_decode.setText('Расшифровать')
+            self.but_act_encode.setText('З')
+            self.line_inp_text.setText('')
+            self.line_out_text.setText('')
+            self.line_inp_key.setText('')
+
+
+
+        elif self.action == "Расшифровать":
+            self.action = "Зашифровать"
+
+            #Animation for button action decode
+            self.anim_but_act_decode = QPropertyAnimation(self.but_act_decode, b"geometry")
+            self.anim_but_act_decode.setEndValue(QRect(426, 4, 36, 36))
+            self.anim_but_act_decode.setDuration(200)
+
+            #Animation for button action encode
+            self.anim_but_act_encode = QPropertyAnimation(self.but_act_encode, b"geometry")
+            self.anim_but_act_encode.setEndValue(QRect(178, 4, 250, 36))
+            self.anim_but_act_encode.setDuration(200)
+
+            #Animation for line input key
+            self.anim_line_inp_key = QPropertyAnimation(self.line_inp_key, b"geometry")
+            self.anim_line_inp_key.setEndValue(QRect(60, 119, 520, 36))
+            self.anim_line_inp_key.setDuration(200)
+
+            #Animation for button action
+            self.anim_but_act = QPropertyAnimation(self.but_act, b"geometry")
+            self.anim_but_act.setEndValue(QRect(60, 159, 520, 36))
+            self.anim_but_act.setDuration(200)
+
+            #starting animations
+            self.anim_but_act_encode.start()
+            self.anim_but_act_decode.start()
+            self.anim_line_inp_key.start()
+            self.anim_but_act.start()
+
+            #moving line_out_key and line_out_text off the screen
+            self.line_out_text.move(60, 386)
+            self.line_out_key.move(60, 497)
+
+            #set text
+            self.but_act_encode.setText('Зашифровать')
+            self.but_act_decode.setText('Р')
+            self.line_inp_text.setText('')
+            self.line_out_text.setText('')
+            self.line_inp_key.setText('')
+
+    def btnHistory(self):
+        self.but_history = QPropertyAnimation(self.history, b"geometry")
+        self.but_history.setEndValue(QRect(0, 0, 320, 386))
+        self.but_history.setDuration(200)
+
+        self.dark_background.show()
+        self.but_history.start()
+
+    def btnClose(self):
+        self.but_history = QPropertyAnimation(self.history, b"geometry")
+        self.but_history.setEndValue(QRect(-320, 0, 320, 386))
+        self.but_history.setDuration(200)
+
+        self.dark_background.hide()
+        self.but_history.start()
+
+    def encodeDecode(self):
+        if self.action == "Зашифровать":
+            #Animation for text out line
+            self.anim_line_out_text = QPropertyAnimation(self.line_out_text, b"geometry")
+            self.anim_line_out_text.setEndValue(QRect(60, 199, 520, 107))
+            self.anim_line_out_text.setDuration(200)
+
+            #Animation for key out line
+            self.anim_line_out_key = QPropertyAnimation(self.line_out_key, b"geometry")
+            self.anim_line_out_key.setEndValue(QRect(60, 310, 520, 36))
+            self.anim_line_out_key.setDuration(200)
+
+            #starting animations
+            self.anim_line_out_text.start()
+            self.anim_line_out_key.start()
+
+        if self.action == "Расшифровать":
+            #Animation for text out line
+            self.anim_line_out_text = QPropertyAnimation(self.line_out_text, b"geometry")
+            self.anim_line_out_text.setEndValue(QRect(60, 239, 520, 107))
+            self.anim_line_out_text.setDuration(200)
+
+            #starting animations
+            self.anim_line_out_text.start()
 
 
 
--- Sources/Gui/Gui_py.py
+++ Sources/Gui/Gui_py.py
@@ -13,27 +13,133 @@ from PyQt5.QtGui import QPixmap
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        #Main Screen
+        #############################################################
+        #initial values and constants
+        self.n = 172
+        self.selected_shifr = "Шифр Цезаря"
+        self.action = "Зашифровать"
+        self.how_work_is = "Как работает " + self.selected_shifr
+
+        #############################################################
+        #main screen
         MainWindow.setObjectName("MainWindow")
         MainWindow.setFixedSize(640, 386)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        #background picture
-        pixmap = QPixmap("Sources/Picture/background.png")
-        label = QtWidgets.QLabel(self.centralwidget)
-        label.setGeometry(QtCore.QRect(0, 0, 640, 386))
-        label.setPixmap(pixmap)
+        #############################################################
+        #BACKGROUND PICTURE
 
-        #Menubar
-        menu = QtWidgets.QWidget(self.centralwidget)
-        menu.setGeometry(QtCore.QRect(0, 0, 640, 40))
-        menu.setObjectName("menubar")
+        self.pixmap = QPixmap("Sources/Picture/background.png")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 640, 386))
+        self.label.setPixmap(self.pixmap)
+
+        #############################################################
+        #DARK BACKGROUND
+
+        self.dark_background = QtWidgets.QPushButton(self.centralwidget)
+        self.dark_background.setGeometry(QtCore.QRect(0, 0, 640, 386))
+        self.dark_background.setObjectName("dark_background")
+        self.dark_background.hide()
+
+        #############################################################
+        #MENUBAR
+
+        #menu place
+        self.menu = QtWidgets.QWidget(self.centralwidget)
+        self.menu.setGeometry(QtCore.QRect(0, 0, 640, 44))
+        self.menu.setObjectName("menubar")
+
+        #history button
+        self.but_history = QtWidgets.QPushButton(self.menu)
+        self.but_history.setGeometry(QtCore.QRect(4, 4, 70, 36))
+
+        #action encode
+        self.but_act_encode = QtWidgets.QPushButton(self.menu)
+        self.but_act_encode.setGeometry(QtCore.QRect(178, 4, 250, 36))
+
+        #action decode
+        self.but_act_decode = QtWidgets.QPushButton(self.menu)
+        self.but_act_decode.setGeometry(QtCore.QRect(426, 4, 36, 36))
+
+        #dropdown menu shifr
+        self.select_shifr = QtWidgets.QComboBox(self.menu)
+        self.select_shifr.setGeometry(QtCore.QRect(506, 4, 130, 36))
+        self.select_shifr.addItem("Шифр Цезаря")
+        self.select_shifr.addItem("Шифр Виженера")
+        self.select_shifr.addItem("Шифр Энигмы")
+        self.select_shifr.addItem("Настоящие кода")
+        self.select_shifr.addItem("Публичный ключ")
+        self.select_shifr.activated[str].connect(self.set_text_how_work_is)
+
+
+        #############################################################
+        #MAIN SCREEN
+
+        #key input
+        self.line_inp_key = QtWidgets.QLineEdit(self.centralwidget)
+        self.line_inp_key.setGeometry(QtCore.QRect(60, 119, 520, 36))
+        self.line_inp_key.setPlaceholderText("Введите ключ")
+
+        #text input
+        self.line_inp_text = QtWidgets.QTextEdit(self.centralwidget)
+        self.line_inp_text.setGeometry(QtCore.QRect(60, 48, 520, 107))
+        self.line_inp_text.setPlaceholderText("Введите текст")
+
+        #button action
+        self.but_act = QtWidgets.QPushButton(self.centralwidget)
+        self.but_act.setGeometry(QtCore.QRect(60, 159, 520, 36))
+
+        #text output
+        self.line_out_text = QtWidgets.QTextEdit(self.centralwidget)
+        self.line_out_text.setGeometry(QtCore.QRect(60, 386, 520, 107))
+
+        #key output
+        self.line_out_key = QtWidgets.QTextEdit(self.centralwidget)
+        self.line_out_key.setGeometry(QtCore.QRect(60, 497, 520, 36))
+
+        #button how
+        self.but_how_work = QtWidgets.QPushButton(self.centralwidget)
+        self.but_how_work.setGeometry(QtCore.QRect(60, 350, 520, 36))
+
+        #############################################################
+        #HISTORY
+
+        #history place
+        self.history = QtWidgets.QWidget(self.centralwidget)
+        self.history.setGeometry(QtCore.QRect(-320, 0, 320, 500))
+        self.history.setObjectName("history")
+
+        self.history_box = QtWidgets.QVBoxLayout(self.history)
+        self.history_box.setGeometry(QtCore.QRect(0, 0, 316, 0))
+        self.history_box.setContentsMargins(2, 2, 2, 2)
+        self.history_box.setSpacing(8)
+
+        self.but_close = QtWidgets.QPushButton(self.history)
+        self.but_close.setGeometry(QtCore.QRect(2, 2, 70, 36))
+
+        self.but_clear = QtWidgets.QPushButton(self.history)
+        self.but_clear.setGeometry(QtCore.QRect(74, 2, 70, 36))
+
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def set_text_how_work_is(self, text_how_work_is):
+        '''changes the name of the cipher for the bot_how_work button'''
+        self.selected_shifr = self.select_shifr.currentText()
+        self.how_work_is = "Как работает " + self.selected_shifr
+        self.but_how_work.setText(self.how_work_is)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Crypto Machine"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.but_act_encode.setText(_translate("MainWindow", "Зашифровать"))
+        self.but_act_decode.setText(_translate("MainWindow", "Р"))
+        self.but_act.setText(_translate("MainWindow", self.action))
+        self.but_history.setText(_translate("MainWindow", "История"))
+        self.but_how_work.setText(_translate("MainWindow", self.how_work_is))
+        self.but_close.setText(_translate("MainWindow", "Закрыть"))
+        self.but_clear.setText(_translate("MainWindow", "Очистить"))
