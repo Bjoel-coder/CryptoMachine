cb6f09d2431b49832e2dac8fb54e4661d5c04cf1
Bjoel-coder <bjoel.cuper@mail.ru>
2020-11-10 20:10:22 +0300

    little changes in Caesar shifr

--- Moduls/Caesar_shifr/Caesar_shifr.py
+++ Moduls/Caesar_shifr/Caesar_shifr.py
@@ -11,53 +11,32 @@ class Caesar:
     lib = (symbolsRus, symbolsEng, numbers)
 
     def encode(self, text_input, key_input):
-        if text_input != "":
-            try:
-                key  = int(key_input)
-                if (key >= 0) and (key <= 100):
-                    text = list(text_input)
-                    count = len(text)
-                    for i in range(count):
-                        alphabet = None
-                        for j in range(3):
-                            if text[i] in self.lib[j]:
-                                alphabet = self.lib[j]
-                                len_alphabet = len(alphabet)
-                                break
-                        if alphabet != None:
+        return self.shifr("encode", text_input, key_input)
+
+    def decode(self, text_input, key_input):
+        return self.shifr("decode", text_input, key_input)
+
+    def shifr(self, action, text_input, key_input):
+        if text_input == "":
+            return "Введите текст!", "", "#$/ERROR/$#"
+        try:
+            key  = int(key_input)
+            if (key >= 0) and (key <= 100):
+                text = list(text_input.lower())
+                count = len(text)
+                for i in range(count):
+                    alphabet = None
+                    for j in range(3):
+                        if text[i] in self.lib[j]:
+                            alphabet = self.lib[j]
+                            len_alphabet = len(alphabet)
+                            break
+                    if alphabet != None:
+                        if action == "encode":
                             ind = alphabet.index(text[i]) + key
                             ind = ind%len_alphabet
                             text[i] = alphabet[ind]
                         else:
-                            pass
-
-                    text = "".join(text)
-                    text_output = str(text)
-                    key_output = str(key)
-                    return text_output, "", None
-                else:
-                    return "Ключ должен быть числом >=0 и <=100!", "", "#$/ERROR/$#"
-            except:
-                return "Ключ должен быть числом >=0 и <=100!", "", "#$/ERROR/$#"
-        else:
-            return "Введите текст!", "", "#$/ERROR/$#"
-
-
-    def decode(self, text_input, key_input):
-        if text_input != "":
-            text = list(text_input)
-            try:
-                key = int(key_input)
-                if (key >= 0) and (key <= 100):
-                    count = len(text)
-                    for i in range(count):
-                        alphabet = None
-                        for j in range(3):
-                            if text[i] in self.lib[j]:
-                                alphabet = self.lib[j]
-                                len_alphabet = len(alphabet)
-                                break
-                        if alphabet != None:
                             key = key%len_alphabet
                             ind = alphabet.index(text[i]) - key
                             if ind<0:
@@ -65,17 +44,14 @@ class Caesar:
                                 text[i] = alphabet[ind]
                             else:
                                 text[i] = alphabet[ind]
-                        else:
-                            pass
-                    text = "".join(text)
-                    text_output = str(text)
-                    return text_output, "", None
-                else:
-                    return "Ключ должен быть числом >=0 и <=100!", "", "#$/ERROR/$#"
-            except:
-                if key_input == "":
-                    return "Введите ключ!", None, "#$/ERROR/$#"
-                else:
-                    return "Ключ должен быть числом >=0 и <=100!", None, "#$/ERROR/$#"
-        else:
-            return "Введите текст!", "", "#$/ERROR/$#"
+                    else:
+                        pass
+
+                text = "".join(text)
+                text_output = str(text)
+                key_output = str(key)
+                return text_output, "", None
+            else:
+                return "Ключ должен быть числом >=0 и <=100!", "", "#$/ERROR/$#"
+        except:
+            return "Ключ должен быть числом >=0 и <=100!", "", "#$/ERROR/$#"
